2c4daaa21782541c17c5d761f80b250a79fd7677
casiforce <genezyp.przetrawiony@protonmail.com>
2025-11-16 17:41:26 +0100

    redis_client

--- app/config.py
+++ app/config.py
@@ -8,4 +8,10 @@ DATABASE_URL = os.getenv(
     "postgresql://taskqueue:taskqueue123@localhost:5432/taskqueue_db"
 )
 
-print(f"DATABASE_URL loaded: {DATABASE_URL}") 
+REDIS_URL = os.getenv(
+    "REDIS_URL",
+    "redis://localhost:6379"
+)
+
+print(f"DATABASE_URL loaded: {DATABASE_URL}")
+print(f"REDIS_URL loaded: {REDIS_URL}")
---
+++ app/redis_client.py
@@ -0,0 +1,24 @@
+import redis
+from typing import Optional
+from app.config import REDIS_URL
+
+redis_client = redis.from_url(REDIS_URL, decode_responses=True)
+
+
+def push_task(task_id: int, priority: int = 3):
+    if not 1 <= priority <= 5:
+        raise ValueError("Priority must be 1-5")
+    
+    redis_client.lpush(priority, task_id)
+
+
+def pop_task(timeout: int = 5) -> Optional[int]:
+    queues = [5, 4, 3, 2, 1]
+    
+    result = redis_client.brpop(queues, timeout=timeout)
+    
+    if result:
+        queue_name, task_id = result
+        return int(task_id)
+    
+    return None
